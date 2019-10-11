bce8e19cd5c0dbf69ce84afcb7eca3e6527f04f9
Justin Barry <328965+justinbarry@users.noreply.github.com>
2019-10-11 02:41:07 -0700

    Add run history table and style it

--- src/components/scenario-item.tsx
+++ src/components/scenario-item.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Elevation } from "@blueprintjs/core";
 import * as React from "react";
+import "./scenario-item.css";
 
 interface IScenarionCompletedRun {
   id: string;
@@ -15,11 +16,30 @@ export interface IScenarioItemProps {
 }
 
 export const ScenarioItem = ({ description, isRunning, name, runHistory, startScenario }: IScenarioItemProps) => (
-  <Card interactive={true} elevation={Elevation.ZERO}>
-    <h5>{name}</h5>
-    <p>{description}</p>
-    <Button disabled={isRunning} onClick={startScenario}>Submit</Button>
-
-    {runHistory.map((rh) => <div key={rh.id}>{rh.duration}</div>)}
+  <Card className="scenario-item" interactive={true} elevation={Elevation.ZERO}>
+    <div className="left">
+      <h5>{name}</h5>
+      <p>{description}</p>
+      <Button disabled={isRunning} onClick={startScenario}>Submit</Button>
+    </div>
+    <div className="right">
+      <table
+        className="bp3-html-table bp3-interactive bp3-small bp3-html-table-striped bp3-html-table-condensed bp3-html-table-bordered">
+        <thead>
+        <tr>
+          <th>Run</th>
+          <th>Duration</th>
+        </tr>
+        </thead>
+        <tbody>
+        {runHistory.map((rh, index) =>
+          <tr key={rh.id}>
+            <td>{index + 1}</td>
+            <td>{rh.duration}</td>
+          </tr>
+        )}
+        </tbody>
+      </table>
+    </div>
   </Card>
 );
